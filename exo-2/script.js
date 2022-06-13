934da9fce7fd3197df52944d85135e554dbef474
Sylvain Rotureau <sylvainrotureau@yahoo.fr>
2022-06-13 15:59:59 +0200

    exo 2 en cours

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,36 @@
 // you can write js here
 console.log('exo-2');
+
+// Déclaration de variables
+let runnerAge = prompt("Quel est votre âge ?");
+let runnerRegistered = prompt("Etes vous déjà inscrit ? OUI ou NON");
+let runnerNumber = Math.floor(Math.random() * 1000);
+let message = "";
+
+// Transformation du type de variable en intéger
+runnerAge = parseInt(runnerAge);
+
+// Vérification de l'inscription du coureur
+if (runnerRegistered === "OUI") {
+    runnerRegistered = true;
+} else if (runnerRegistered === "NON") {
+    runnerRegistered = false;
+    runnerNumber = runnerNumber + 1000;
+} else {
+    console.log("Erreur : la réponse à la deuxième question n'est ni OUI ni NON")
+}
+
+// Vériication de l'age du coureur
+// création du message
+if (runnerAge > 18 && runnerRegistered) {
+    message = "you will race at 9:30am."
+} else if (runnerAge > 18 || runnerRegistered) {
+    message = "you will race at 11:00am."
+} else if (runnerAge < 18 || !runnerRegistered) {
+    message = "you will race at 12:30am."
+} else {
+    message = "Go to see the registration desk"
+}
+
+// Affichage du message dans la console
+console.log(message);
